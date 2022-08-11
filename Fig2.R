e77ac867db2e1bbb8e9e66bfcff37c363edb4c33
Bingzhang Chen <bingzhang.chen@gmail.com>
2022-08-11 12:02:38 +0100

    Updating Fig2.R to satisfy Am Nat requirement

--- Fig2.R
+++ Fig2.R
@@ -19,11 +19,11 @@ plot_Ea_example <- function(dat, XLAB = ''){
   IDs <- sample(IDs, N)
   for (i in 1:N){
     tmp <- dat %>% subset(ID == IDs[i])
-    points(tmp$X, log(tmp$Growth), cex=.5, col=i)
+    points(tmp$X, log(tmp$Growth), cex=.5, col=i, pch=i)
     z <- lm(log(Growth) ~ X, tmp[tmp$Growth > 0, ])
     newx <- data.frame(X=seq(min(tmp$X), max(tmp$X), .01))
     newy <- predict(z, newdata=newx)
-    lines(newx$X, newy, col=i, lwd=.5)
+    lines(newx$X, newy, col=i, lwd=.5, lty=i)
   }
 }
 
@@ -64,8 +64,15 @@ plot_Ea_hist <- function(oridat){
 Ncol  <- 10  #10 color codes
 Topts <- seq(min(PEuk2$XoptL)-.01, max(PEuk2$XoptL)+.01, length.out=Ncol)
 ToptC <- round(revTK(Topts),0) 
+
+#Point colors
 jet.colors   <- colorRampPalette(c("#00007F", "blue", "#007FFF", "cyan",
                                     "#7FFF7F", "yellow", "#FF7F00", "red", "#7F0000"))  #A good color
+
+
+#Symbol shapes
+Shapes <- 16:(15+Ncol)
+
 plot_Einter <- function(oridat, XLAB = '', plot.legend = FALSE){
   oridat <- oridat %>% subset(Growth > 0)
   unidat <- ddply(oridat, .(ID), summarize,
@@ -76,7 +83,7 @@ plot_Einter <- function(oridat, XLAB = '', plot.legend = FALSE){
  #Assign color code for optimal temperature (XoptL)
  unidat$Col_code <- NA
  
-  for (i in 1:nrow(unidat)){
+ for (i in 1:nrow(unidat)){
        code              <- unidat$ID[i]
        tmp               <- oridat[oridat$ID == code,]
        unidat[i, 'mj'  ] <- nrow(tmp)
@@ -99,7 +106,7 @@ plot_Einter <- function(oridat, XLAB = '', plot.legend = FALSE){
  
   #Plot
   plot(unidat$Tbar, unidat$bj, 
-       pch  = 16,
+       pch  = Shapes[unidat$Col_code],
        col  = jet.colors(Ncol)[unidat$Col_code],
        cex  = 0.6,
        xaxs ='i',
@@ -107,13 +114,17 @@ plot_Einter <- function(oridat, XLAB = '', plot.legend = FALSE){
        xlim = c(-2.5,4),
        ylim = YLIM,
        xlab = XLAB,
-       ylab = expression(paste(italic(b))))
+       ylab = expression(paste(italic(b) * ', dimensionless')))
   abline(Ee)
   
   #Add color legend
   if (plot.legend){
       txt <- paste(ToptC, 'ºC')
-      legend('bottomleft', legend=txt, col=jet.colors(Ncol), cex=.6,pch=16)
+      legend('bottomleft', 
+             legend = txt, 
+             col = jet.colors(Ncol), 
+             cex = .6,
+             pch = Shapes)
       text(-2.4, -0.36, bquote(T[opt]), cex=.6, pos=4)   
   }
 }
@@ -151,7 +162,7 @@ plot_EL <- function(oridat, XLAB = '', plot.legend = FALSE){
  
   #Plot
   plot(unidat$XoptL, unidat$ym, 
-       pch  = 16,
+       pch  = Shapes[unidat$Col_code],
        col  = jet.colors(Ncol)[unidat$Col_code],
        cex  = 0.6,
        xaxs ='i',
@@ -159,13 +170,17 @@ plot_EL <- function(oridat, XLAB = '', plot.legend = FALSE){
        xlim = c(-2.5,4),
        ylim = YLIM,
        xlab = XLAB,
-       ylab = expression(paste(italic(y[m]))))
+       ylab = expression(paste(italic(y[m]) * ', dimensionless')))
   abline(EL)
   
   #Add color legend
   if (plot.legend){
       txt <- paste(ToptC, 'ºC')
-      legend('bottomleft', legend=txt, col=jet.colors(Ncol), cex=.6,pch=16)
+      legend('bottomleft', 
+             legend = txt, 
+             col = jet.colors(Ncol), 
+             cex = .6,
+             pch = Shapes)
       text(-2.2, -0.68, bquote(T[opt]), cex=.6, pos=4)   
   }
 }
@@ -179,16 +194,16 @@ par(font.lab  = 1,
     mar       = c(3,4,1,.2),
     fig       = c(0,1,0,1)
    )
-x1   <- c(0, 15)
-y1   <- T.K(x1)
+x1 <- c(0, 15)
+y1 <- T.K(x1)
 
-#All eukaryotic phytoplankton
+#All eukaryotic autotrophic protists
 par(fig = c(0, .5, 0.75, 1), new = T)  
 plot(PEuk2$X, log(PEuk2$Growth), pch = 16, cex=.5,
     xlim = c(-2.5,4),
     ylim = YLIM,
-    xlab = expression(paste('1/'*italic(k[b])*'(1/'*italic(T[r])*' - 1/'*italic(T)*') or '*italic(x))),
-    ylab = expression(paste(italic(ln)*' growth rate ') ) )
+    xlab = expression(paste('1/'*italic(k[b])*'(1/'*italic(T[r])*' - 1/'*italic(T)*') or '*italic(x) *' ('*eV^-1*')')),
+    ylab = expression(paste(italic(ln)*' growth rate (dimensionless)') ) )
 points(PEuk[PEuk$right,]$X, 
        log(PEuk[PEuk$right,]$Growth), pch = 16, col = 'gray', cex=.5)
 z <- lm(log(Growth) ~ X, PEuk2[PEuk2$Growth > 0, ])
@@ -202,7 +217,7 @@ par(fig = c(0.5, 1, 0.75, 1), new = T)
 plot(zoodat2$X, log(zoodat2$Growth), pch = 16,  cex=.5,
     xlim = c(-2.5,4),
     ylim = YLIM,
-    xlab = expression(paste('1/'*italic(k[b])*'(1/'*italic(T[r])*' - 1/'*italic(T)*') or '*italic(x))),
+    xlab = expression(paste(italic(x) *' ('*eV^-1*')')),
     ylab = expression(paste(italic(ln)*' growth rate ') ) )
 points(zoodat[zoodat$right,]$X, log(zoodat[zoodat$right,]$Growth), pch = 16, col = 'gray', cex=.5)
 zoo.lm0 <- lm(log(Growth) ~ X, zoodat2[zoodat2$Growth > 0, ])
@@ -211,7 +226,7 @@ abline(zoo.lm0)
 mtext(expression(bold(B)*') Heterotrophs '*italic(E[app])), side=3, adj=0)
 
 par(fig = c(0, .5, 0.5, .75), new = T)  
-plot_Ea_example(PEuk2, XLAB = expression(paste(italic(x) ) ) )
+plot_Ea_example(PEuk2, XLAB = expression(paste(italic(x) *' ('*eV^-1*')' ) ) )
 
 mtext(expression(bold(C)*') Autotrophs '*italic(E[intra])), adj=0)
 par(fig = c(0.22,.48, 0.53, .7), new = T)  #Plot inset
@@ -220,7 +235,7 @@ plot_Ea_hist(PEuk2)
 
 par(mgp=c(2.2,1,0))
 par(fig = c(0.5, 1, 0.5,.75), new = T)  
-plot_Ea_example(zoodat2, XLAB = expression(paste(italic(x) ) ) ) 
+plot_Ea_example(zoodat2, XLAB = expression(paste(italic(x) *' ('*eV^-1*')') ) ) 
 mtext(expression(bold(D)*') Heterotrophs '*italic(E[intra])), adj=0)
 par(fig = c(0.72,.98, 0.53, .7), new = T)  #Plot inset
 par(mgp=c(1,.1,0))
@@ -229,21 +244,22 @@ plot_Ea_hist(zoodat2)
 #Plot out Einter
 par(mgp=c(2.2,1,0))
 par(fig = c(0, 0.5, 0.25, 0.5), new = T)  
-plot_Einter(PEuk2, XLAB = expression(paste(italic(bar(x)))) )
+plot_Einter(PEuk2, XLAB = expression(paste(italic(bar(x)) * ' ('*eV^-1*')' )) )
 mtext(expression(bold(E)*') Autotrophs '*italic(E[inter])), adj=0)
 
 par(fig = c(0.5, 1, 0.25, 0.5), new = T)  
-plot_Einter(zoodat2, XLAB = expression(paste(italic(bar(x)))), plot.legend = T )
+plot_Einter(zoodat2, XLAB = expression(paste(italic(bar(x)) * ' ('*eV^-1*')')),
+            plot.legend = T )
 mtext(expression(bold(F)*') Heterotrophs '*italic(E[inter])), adj=0)
 
 #Plot out EL
 par(fig = c(0, 0.5, 0, 0.25), new = T)  
-plot_EL(PEuk2, XLAB = expression(paste(italic(x[m])))  )
+plot_EL(PEuk2, XLAB = expression(paste(italic(x[m]) *' ('*eV^-1*')' ))  )
 
 mtext(expression(bold(G)*') Autotrophs '*italic(E[L])), adj=0)
 
 par(fig = c(0.5, 1, 0, 0.25), new = T)  
-plot_EL(zoodat2, XLAB = expression(paste(italic(x[m])))    ) 
+plot_EL(zoodat2, XLAB = expression(paste(italic(x[m]) *' ('*eV^-1*')' )) ) 
 
 mtext(expression(bold(H)*') Heterotrophs '*italic(E[L])), adj=0)
 
